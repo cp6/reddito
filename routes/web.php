5be4f1ca28b1d3de729e849b6a5d151b21a7bcda
cp6 <cp6@users.noreply.github.com>
2023-01-02 16:52:49 +1100

    Added routes for processing and getting counts

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Fetch new posts
 Route::get('/call/get-posts', [App\Http\Controllers\FetchController::class, 'get'])->name('call.get');
+
+//Post process queue for post counts
+Route::get('/call/post-queue/author/{amount?}', [App\Http\Controllers\PostProcessQueueController::class, 'doAuthorCounts'])->name('call.post-queue.author');
+Route::get('/call/post-queue/sub/{amount?}', [App\Http\Controllers\PostProcessQueueController::class, 'doSubCounts'])->name('call.post-queue.sub');
+Route::get('/call/post-queue/domain/{amount?}', [App\Http\Controllers\PostProcessQueueController::class, 'doDomainCounts'])->name('call.post-queue.domain');
+
+//Get total comment counts
+Route::get('/call/count/comments/author/{domain}', [App\Http\Controllers\AuthorController::class, 'countComments'])->name('call.count.comments.author');
+Route::get('/call/count/comments/sub/{domain}', [App\Http\Controllers\SubController::class, 'countComments'])->name('call.count.comments.sub');
+Route::get('/call/count/comments/domain/{domain}', [App\Http\Controllers\DomainController::class, 'countComments'])->name('call.count.comments.domain');
+
+//Get total score (up votes)
+Route::get('/call/count/score/author/{domain}', [App\Http\Controllers\AuthorController::class, 'countScore'])->name('call.count.score.author');
+Route::get('/call/count/score/sub/{domain}', [App\Http\Controllers\SubController::class, 'countScore'])->name('call.count.score.sub');
+Route::get('/call/count/score/domain/{domain}', [App\Http\Controllers\DomainController::class, 'countScore'])->name('call.count.score.domain');
