23ca270611dedaa11c7a28d3903f7a1754e8f27b
cp6 <cp6@users.noreply.github.com>
2023-01-04 15:50:15 +1100

    Updated AwardsForPost::do() to be static

--- app/Models/AwardsForPost.php
+++ app/Models/AwardsForPost.php
@@ -21,7 +21,7 @@ class AwardsForPost extends Model
         return $this->hasOne(Post::class, 'id', 'post_id');
     }
 
-    public function do(string $post_id, array $award): AwardsForPost
+    public static function do(string $post_id, array $award): AwardsForPost
     {
         return self::updateOrCreate(['post_id' => $post_id, 'award_id' => $award['id']], [
             'count' => $award['count']
